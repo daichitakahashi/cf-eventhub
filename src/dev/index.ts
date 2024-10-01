e1bf54c58e8efa3a9f805ff2b460baa1f77a73d8
daichitakahashi <t.daichi.snd@gmail.com>
2024-10-02 01:28:04 +0900

    feat: add EventHub for development

---
+++ src/dev/index.ts
@@ -0,0 +1,57 @@
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+import { type Executor as BaseExecutor, Dispatcher } from "../core/executor";
+import {
+  type EventHub as BaseEventHub,
+  EventConsumer,
+  EventSink,
+  getExecutor,
+  getQueue,
+  getRouteConfig,
+} from "../core/hub";
+import type { EventPayload, QueueMessage } from "../core/type";
+import { DevRepository } from "./repository";
+
+export class Executor<
+    Env extends Record<string, unknown> = Record<string, unknown>,
+  >
+  extends WorkerEntrypoint<Env>
+  implements BaseExecutor
+{
+  private dispatcher: Dispatcher;
+
+  constructor(ctx: ExecutionContext, env: Env) {
+    super(ctx, env);
+    const repo = new DevRepository();
+    this.dispatcher = new Dispatcher(repo, env);
+  }
+
+  dispatch(msg: QueueMessage) {
+    return this.dispatcher.dispatch(msg);
+  }
+}
+
+export class EventHub<
+    Env extends Record<string, unknown> = Record<string, unknown>,
+  >
+  extends WorkerEntrypoint<Env>
+  implements BaseEventHub
+{
+  private sink: EventSink;
+  private consumer: EventConsumer;
+
+  constructor(ctx: ExecutionContext, env: Env) {
+    super(ctx, env);
+    const repo = new DevRepository();
+    this.sink = new EventSink(repo, getQueue(env), getRouteConfig(env));
+    this.consumer = new EventConsumer(getExecutor(env));
+  }
+
+  putEvent(events: EventPayload[]) {
+    return this.sink.putEvent(events);
+  }
+
+  queue(batch: MessageBatch<QueueMessage>) {
+    return this.consumer.queue(batch);
+  }
+}
