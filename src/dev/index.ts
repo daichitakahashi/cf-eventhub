56aea9c902c0d58cf9baabd137a87d8df873d5c0
daichitakahashi <t.daichi.snd@gmail.com>
2024-11-10 18:02:36 +0900

    fix: make `dev/EventHub` dev-adopted again
    This worker is both Event producer and consumer

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -2,11 +2,10 @@ import type { Executor as BaseExecutor } from "../core/executor";
 import { RpcExecutor } from "../core/executor/rpc";
 import { RpcEventHub } from "../core/hub/rpc";
 import type { Repository } from "../core/repository";
+import type { QueueMessage } from "../core/type";
 import { DevRepository } from "./repository";
 
-export class Executor<
-    Env extends Record<string, unknown> = Record<string, unknown>,
-  >
+class Executor<Env extends Record<string, unknown> = Record<string, unknown>>
   extends RpcExecutor<Env>
   implements BaseExecutor
 {
@@ -18,7 +17,15 @@ export class Executor<
 export class EventHub<
   Env extends Record<string, unknown> = Record<string, unknown>,
 > extends RpcEventHub<Env> {
+  private executor: Executor<Env>;
+  constructor(ctx: ExecutionContext, env: Env) {
+    super(ctx, env);
+    this.executor = new Executor(ctx, env);
+  }
   protected getRepository(): Repository {
     return new DevRepository();
+  }
+  queue(batch: MessageBatch<QueueMessage>) {
+    return this.executor.queue(batch);
   }
 }
