9a22301916574eed4bd13491412b511f23dd2246
daichitakahashi <t.daichi.snd@gmail.com>
2024-11-26 18:35:13 +0900

    feat: new separated "@cf-eventhub/pulumi" package

---
+++ .changeset/famous-scissors-swim.md
@@ -0,0 +1,6 @@
+---
+"cf-eventhub": patch
+"@cf-eventhub/pulumi": patch
+---
+
+New separated "@cf-eventhub/pulumi" package
--- cf-eventhub/src/core/index.ts
+++ cf-eventhub/src/core/index.ts
@@ -1,5 +1,6 @@
 export * from "./logger";
 export * from "./executor";
 export * from "./hub";
+export * from "./hub/routing";
 export * from "./repository";
 export * from "./type";
